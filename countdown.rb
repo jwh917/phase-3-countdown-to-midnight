01721949d898a3800915ff0a1988aa3deaf0f590
Julien Hnery <jwh_917@yahoo.com>
2022-08-18 22:20:40 -0400

    Countdown_with_sleep and Countdown method

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown(int)
+  countdown_with_sleep(int)
 end
